7e5abcf58c981debe8f712e7ebc8bd8a62a6fa4e
feldan <feldan@outlook.com>
2022-11-18 19:49:25 +0800

    fix

--- js/register copy.js
+++ js/register copy.js
@@ -1,22 +1,37 @@
-$.post("https://www.xiaohao2333.top:25566/v1/verify-codes/captcha", function (jsondate) {
-    let images = jsondate.data.captcha_image;
-    let imagesId = jsondate.data.captcha_id
-    console.log(jsondate.data.captcha_id)
-    $("#captcha_image").attr('src', images);
-    $('#btn').click(function () {
-        $.post("https://www.xiaohao2333.top:25566/v1/verify-codes/reg", data = {
-            name: $('#name').val(),
-            id: $('#id').val(),
-            gender: $('#gender').val(),
-            phone: $('#phone').val(),
-            email: $('#email').val(),
-            reason: $('#selector').val(),
-            code: $('#code').val(),
-            captcha_id: imagesId,
-            captcha_answer: $('#captcha_answer').val()
-        }, function (registerdate) {
+function GetJsonData() {
+    var json = {
+        name: $('#name').val(),
+        id: $('#id').val(),
+        gender: $('#gender').val(),
+        phone: $('#phone').val(),
+        email: $('#email').val(),
+        reason: $('#selector').val(),
+        code: $('#code').val(),
+        captcha_answer: $('#captcha_answer').val()
+    };
+    return json;
+}
+console.log(GetJsonData());
+$('#btn').click(function () {
+    $.ajax({
+        type: "POST",
+        url: "https://www.xiaohao2333.top:30000/v1/verify-codes/reg",
+        data: JSON.stringify(GetJsonData()),
+        //  {
+        //     name:$('#name').val(),
+        //     id:$('#id').val(),
+        //     gender:$('#gender').val(),
+        //     phone:$('#phone').val(),
+        //     email:$('#email').val(),
+        //     reason:$('#selector').val(),
+        //     code:$('#code').val(),
+        //     captcha_id:imagesId,
+        //     captcha_answer:$('#captcha_answer').val()
+        // },
+        dataType: "JSON",
+        success: function (registerdate) {
             console.log();
             alert("信息: " + registerdate);
-        }, JSON)
-    }, JSON);
-})
+        }
+    });
+});
